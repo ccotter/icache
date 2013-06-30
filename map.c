266599178af1063617539fa8be802606bc69065d
Chris Cotter <ccotter@utexas.edu>
2013-06-30 18:28:59 -0400

    .

--- map.c
+++ map.c
@@ -13,8 +13,6 @@
 SYM(fun)
 SYM(fun_ins)
 SYM(fun_end)
-SYM(fun2_ins)
-SYM(fun2_end)
 
 uint8_t zeros[0x1000];
 int main(void)
@@ -32,36 +30,12 @@ int main(void)
     assert((void*)-1 != a1);
     assert((void*)-1 != a2);
 
-    long sz = DIFF(fun, fun2_end);
+    long sz = DIFF(fun, fun_end);
     memcpy(a1, fun, sz);
 
-    uint64_t *p1 = (uint64_t*)&a2[DIFF(fun, fun_ins)];
-    uint64_t *p2 = (uint64_t*)&a2[DIFF(fun, fun2_ins)];
-    uint64_t old = *p1;
-    uint64_t new = *p2;
-    typedef int (*fn_t)(uint64_t*, uint64_t);
-    uint64_t alt = old;
-    for(;;)
-    {
-        uint64_t val = ((fn_t)a1)(p1, alt);
-        if (alt == new)
-        {
-            if (val == 1)
-            {
-                printf("wrong!\n");
-            }
-            alt = old;
-        }
-        else
-        {
-            if (val == 2)
-            {
-                printf("wrong!\n");
-            }
-            alt = new;
-        }
-    }
-
+    void *p1 = &a2[DIFF(fun, fun_ins)];
+    typedef int (*fn_t)(void*);
+    printf("ret=%d\n", ((fn_t)a1)(p1));
     close(fd);
     return 0;
 }
